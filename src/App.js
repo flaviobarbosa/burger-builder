f2f6a70b78c17100595e8a9c925ec89afaf8629f
flaviobarbosa <flavioramone@gmail.com>
2020-10-28 10:41:47 -0300

    burger builder container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 export default function App() {
   return (
     <div>
       <Layout>
-        <p>layout test</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
