2cc3638bb095431824a5aafbbe6817289e53d937
flaviobarbosa <flavioramone@gmail.com>
2020-10-28 10:24:14 -0300

    Creating layout component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+import Layout from "./components/Layout/Layout";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Layout>
+        <p>layout test</p>
+      </Layout>
+    </div>
+  );
 }
